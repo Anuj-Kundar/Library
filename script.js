d2261465f86151663d2e54a84c57990acb161e81
Anuj Kundar <anujkundar003@gmail.com>
2023-07-23 21:12:54 +0530

    add dom element to script

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// reference to DOM elements
+const modal = document.getElementById("modal");
+const addBooksBtn =document.querySelector(".btn-add-books");
+const form = document.querySelector("form");
+const flexContainer=document.querySelector(".flex-container");
+
+// store books in this array
+let myLibrary = [];
+
+// Book constructor
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
@@ -7,6 +17,10 @@ function Book(title, author, pages, isRead) {
         const readStatus = this.isRead ? "read" : "not read yet";
         return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`
     }
+}
+
+function addBookToLibrary() {
+    
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
