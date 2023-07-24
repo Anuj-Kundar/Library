6c0252b9979258e5340154541b94c919bd33fe3e
Anuj Kundar <anujkundar003@gmail.com>
2023-07-24 23:18:22 +0530

    add card functionality

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// reference to DOM elements
-const modal = document.getElementById("modal");
-const addBooksBtn = document.querySelector(".btn-add-books");
-const form = document.querySelector("form");
-const flexContainer = document.querySelector(".flex-container");
+// // reference to DOM elements
+// const modal = document.getElementById("modal");
+// const addBooksBtn = document.querySelector(".btn-add-books");
+// const form = document.querySelector("form");
+// const flexContainer = document.querySelector(".flex-container");
 
 // store books in this array
-let library = [
+let myLibrary = [
     {
         title: "Red Rising",
         author: "Pierce Brown",
@@ -36,15 +36,39 @@ function addBookToLibrary() {
     library.push(book);
 }
 
-function displaybook() {
+function displayBooks() {
+    const flexContainer=document.querySelector(".flex-container")
      flexContainer.innerHTML=""; //-> will clear if any existing books are present
     
     // creating cards to display books
-    library.forEach((book, index)=>{
+    myLibrary.forEach((book, index)=>{
         const card = createCard(book,index);
         flexContainer.appendChild(card);
     });
+}
 
+function createCard(book,index){
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const textGroup=document.createElement("div");
+    textGroup.classList.add("text-group")
+
+    const titleElement=document.createElement("div")
+    titleElement.textContent=book.title;
+
+    const authorElement=document.createElement("div");
+    authorElement.classList.add("text-author");
+    authorElement.textContent=`${book.pages} pages`
+
+    textGroup.appendChild(titleElement);
+    textGroup.appendChild(authorElement);
+    textGroup.appendChild(pagesElement);
+
+    const buttonGroup=document.createElement("div");
+    buttonGroup.classList.add("button-group")
 
 }
+
+
 
